395d75be79585471a115bf4c6bcf7b9d1d3e2501
Tcodingg <princetlive@yahoo.com>
2022-02-05 16:42:26 -0500

    setup drawer navigation. add tabs and show me a joke button

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Tabs from "./navigation/Tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+   createDrawerNavigator,
+   DrawerContentScrollView,
+   DrawerItem,
+   DrawerItemList,
+} from "@react-navigation/drawer";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   const Drawer = createDrawerNavigator();
+
+   return (
+      <NavigationContainer>
+         <StatusBar hidden={false} />
+         <Drawer.Navigator
+            initialRouteName="Tabs"
+            drawerContent={(props) => {
+               return (
+                  <DrawerContentScrollView {...props}>
+                     <DrawerItemList {...props} />
+                     <DrawerItem
+                        label="show me a joke"
+                        onPress={() => console.log("button clicked")}
+                     />
+                  </DrawerContentScrollView>
+               );
+            }}
+         >
+            <Drawer.Screen
+               options={{ headerShown: false }}
+               name="Home"
+               component={Tabs}
+            />
+         </Drawer.Navigator>
+      </NavigationContainer>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+   },
 });
