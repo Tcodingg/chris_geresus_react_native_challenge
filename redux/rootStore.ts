0a769c3d39ae42deef6f4b5bb9514a730c991e35
Tcodingg <princetlive@yahoo.com>
2022-02-06 16:53:41 -0500

    add puchlineReducer into rootReducer

--- redux/rootStore.ts
+++ redux/rootStore.ts
@@ -1,7 +1,9 @@
+import { punchlineReducer } from "./reducers/punchlineReducer";
 import { combineReducers } from "redux";
 import { jokeReducer } from "./reducers/jokeReducer";
 export const rootReducer = combineReducers({
    jokeReducer,
+   punchlineReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
