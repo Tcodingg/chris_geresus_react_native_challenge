f2377084c62639832bb5c740a757b72f0335ce08
Tcodingg <princetlive@yahoo.com>
2022-02-06 15:11:04 -0500

    add expo-notfification dependecy

--- screens/Tab1.tsx
+++ screens/Tab1.tsx
@@ -1,9 +1,20 @@
 import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/rootStore";
 
 const Tab1 = () => {
+   const {
+      newJoke: { setup, delivery, joke },
+   } = useSelector((state: RootState) => state.jokeReducer);
+   console.log(setup, delivery, joke);
+   const [jokeSetup, setJokeSetup] = useState<string | undefined>("");
+   useEffect(() => {
+      setJokeSetup(setup ? setup : joke);
+   }, [setup, joke, delivery]);
    return (
       <View style={styles.container}>
+         <Text>{jokeSetup}</Text>
          <Text>Tab 1</Text>
       </View>
    );
