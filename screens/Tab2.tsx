24ee6cc8f71973f26b88b498293763dcf7284ff5
Tcodingg <princetlive@yahoo.com>
2022-02-07 12:43:17 -0500

    change Tab2 screen to react typescript function componen

--- screens/Tab2.tsx
+++ screens/Tab2.tsx
@@ -1,7 +1,7 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
-const Tab2 = () => {
+const Tab2: React.FC = () => {
    return (
       <View style={styles.container}>
          <Text>Tab 2</Text>
